c4891d8398767ce46067cff369db523f27561b0a
Kirill-68 <kir_54@icloud.com>
2025-01-31 07:31:22 +0300

    Add files via upload

---
+++ crm/api/clients/DeleteProduct.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../DB.php';
+if(isset($_GET['id']) && !empty($_GET['id'])) {
+    $id = $_GET['id'];
+        $delete = $db->query("DELETE FROM `products` WHERE id='$id'")->fetchAll();
+        header('Location: ../../products.php');
+}
+else{
+    header('Location: ../../products.php');
+}
+?>
